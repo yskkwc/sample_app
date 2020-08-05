4665682e3888723cb7856e87d52384adf3fba266
yskkwc <y.k.trial.serv@gmail.com>
2020-08-05 11:52:02 +0000

    Complete advanced testing setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   root 'application#hello'
-=======
-  root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
->>>>>>> static-pages
+
 end
