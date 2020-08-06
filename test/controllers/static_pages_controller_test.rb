c6a22e63a11e90b5fd8f21825aed1911e0be099d
yskkwc <y.k.trial.serv@gmail.com>
2020-08-06 12:04:11 +0000

    Recover Application Helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,30 +4,16 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-<<<<<<< HEAD
-=======
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
->>>>>>> static-pages
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-<<<<<<< HEAD
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
   end
-=======
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
-  end
-  test "should get about" do
-    get static_pages_about_url
-    assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
-  end
-
->>>>>>> static-pages
 end
