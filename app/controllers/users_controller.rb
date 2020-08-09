2cbfb2754fcba23e1fb953688d39d4e5dae4174b
yskkwc <y.k.trial.serv@gmail.com>
2020-08-09 18:18:56 +0900

    Finish cp07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,30 @@
 class UsersController < ApplicationController
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+  end
+  # GET /users/new
   def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample app!"
+      redirect_to @user
+      #GET "/users/#{@user.id}"
+    else
+      render 'new'
+    end
   end
   
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, 
+                                 :email, 
+                                 :password,
+                                 :password_confirmation
+                                 )
+  end
 end
