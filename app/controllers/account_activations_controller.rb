a6dae018c56edb5ccc32e5eb245fbc0c3f1a36c8
yskkwc <y.k.trial.serv@gmail.com>
2020-08-22 01:00:06 +0900

    Change ch11 error of account_act_cont.

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,16 +1,15 @@
 class AccountActivationsController < ApplicationController
+  
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      #nil check, ２回目以降のアクセスを弾く, [:id]にはトークンが入ってる
-      #/account_activations/zr5V1mPDGmMUPUMGsc1dgQ
-      #/edit?email=y.k.trial.serv%40gmail.com
-      user.activate
+      user.update_attribute(:activated,    true)
+      user.update_attribute(:activated_at, Time.zone.now)
       log_in user
       flash[:success] = "Account activated!"
       redirect_to user
     else
-      flash[:danger] = "Invalid activate"
+      flash[:danger] = "Invalid activation link"
       redirect_to root_url
     end
   end
